cd8319353ef7952c4cbe80140f5c48a73ad2063d
MIGU3L <99838970+miguelgustavo583@users.noreply.github.com>
2023-05-29 16:32:05 -0300

    Add files via upload

---
+++ FOR/conexao.php
@@ -0,0 +1,16 @@
+<?php
+    
+    define('MYSQL_HOST', 'localhost:3306');
+    define('MYSQL_USER', 'root');
+    define('MYSQL_PASSWORD', '');
+    define('MYSQL_DB_NAME', 'bd_sistema');
+    
+    try{
+        $PDO = new PDO('mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB_NAME, MYSQL_USER, MYSQL_PASSWORD);
+    }catch (PDOException $e){
+        
+        echo 'Erro ao conectar com o MySQL: ' . $e->getMessage();
+    }
+
+?>
+
